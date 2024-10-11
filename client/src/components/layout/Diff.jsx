ae92d1e34c50abd3502b3a31e16e64bd414ccd67
Matthew Campbell <m.campbell92@gmail.com>
2024-10-11 16:51:27 +0200

    add diff

---
+++ client/src/components/layout/Diff.jsx
@@ -0,0 +1,17 @@
+export default function Diff() {
+  return (
+    <div className="diff aspect-[16/9]">
+      <div className="diff-item-1">
+        <div className="bg-primary text-primary-content grid place-content-center text-9xl font-black">
+          Data insights
+        </div>
+      </div>
+      <div className="diff-item-2">
+        <div className="bg-base-200 grid place-content-center text-9xl font-black">
+          Get data insights relevant to your LGA
+        </div>
+      </div>
+      <div className="diff-resizer"></div>
+    </div>
+  );
+}
