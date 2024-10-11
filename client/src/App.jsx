0eaa83f880ec06783d4cdc16e9b11a73a52686ef
Matthew Campbell <m.campbell92@gmail.com>
2024-10-11 14:21:09 +0200

    import components for testing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,21 @@
-function App() {}
+import Navbar from "./components/layout/Navbar";
+import {
+  ButtonSmall,
+  ButtonMedium,
+  ButtonOutline,
+  ButtonWide,
+} from "./components/ui/Buttons";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <ButtonSmall>Small</ButtonSmall>
+      <ButtonMedium>Medium</ButtonMedium>
+      <ButtonOutline>Outline</ButtonOutline>
+      <ButtonWide>Wide</ButtonWide>
+    </div>
+  );
+}
 
 export default App;
