b9d6925140996bb87a04e6a2dd28c3e4a465d619
Matthew Campbell <m.campbell92@gmail.com>
2024-10-11 16:51:39 +0200

    add demo text

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,27 @@
 import Navbar from "./components/layout/Navbar";
+import HeroSection from "./components/sections/HeroSection";
+import Diff from "./components/layout/Diff";
 import {
   ButtonSmall,
   ButtonMedium,
   ButtonOutline,
-  ButtonWide,
+  ButtonSmallWide,
 } from "./components/ui/Buttons";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <HeroSection />
+      <Diff />
       <ButtonSmall>Small</ButtonSmall>
       <ButtonMedium>Medium</ButtonMedium>
       <ButtonOutline>Outline</ButtonOutline>
-      <ButtonWide>Wide</ButtonWide>
+      <ButtonSmallWide>Wide</ButtonSmallWide>
+      <p className="text-info">Info</p>
+      <p className="text-success">Success</p>
+      <p className="text-warning">Warning</p>
+      <p className="text-error">Error</p>
     </div>
   );
 }
