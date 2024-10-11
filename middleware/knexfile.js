6b229ce64c133576420e96340a46c911ce621d37
Gary Cazzulino <gary@caz.email>
2024-10-11 15:27:46 +1000

    First draft of middleware using Knex to talk to a mysql database with csv data loaded from Localis

---
+++ middleware/knexfile.js
@@ -0,0 +1,9 @@
+module.exports = {
+ client: 'mysql2',
+ connection: {
+ host: '127.0.0.1',
+ database: 'localis_data',
+ user: 'root',
+ password: 'password'
+ }
+}
---
+++ middleware/routes/users.js
@@ -0,0 +1,105 @@
+var express = require('express');
+var router = express.Router();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  res.send('respond with a resource');
+});
+
+/*
+router.post('/login', function(req, res, next) {
+  const expires_in = 60 * 60 * 24;
+  const exp = Math.floor(Date.now() / 1000) + expires_in;
+  const token = jwt.sign({ exp }, process.env.JWT_SECRET);
+  res.status(200).json({
+    token,
+    token_type: "Bearer",
+    expires_in
+  });
+});
+*/
+
+
+router.post('/login', function (req, res, next) {
+  // 1. Retrieve email and password from req.body
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Verify body
+  if (!email || !password) {
+    res.status(400).json({
+      error: true,
+      message: "Request body incomplete - email and password needed"
+    });
+    return;
+  }
+
+  // 2. Determine if user already exists in table
+  const queryUsers = req.db.from("users").select("*").where("email", "=", email);
+  queryUsers
+    .then(users => {
+      if (users.length === 0) {
+        console.log("User does not exist");
+        return;
+      }
+
+      // Compare password hashes
+      const user = users[0];
+      return bcrypt.compare(password, user.hash);
+    })
+    .then(match => {
+      if (!match) {
+        console.log("Passwords do not match");
+        res.status(401).json({
+          error: "Invalid login"
+        });
+        res.end();
+        return;
+      }
+      // Create and return JWT token
+      const expires_in = 60 * 60 * 24; // 24 hours
+      const exp = Math.floor(Date.now() / 1000) + expires_in;
+      const token = jwt.sign({ email, exp }, process.env.JWT_SECRET);
+      res.status(200).json({
+        token,
+        token_type: "Bearer",
+        expires_in
+      });
+    });
+});
+
+router.post('/register', function (req, res, next) {
+  // Retrieve email and password from req.body
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Verify body
+  if (!email || !password) {
+    res.status(400).json({
+      error: true,
+      message: "Request body incomplete - email and password needed"
+    });
+    return;
+  }
+
+  // Determine if user already exists in table
+  const queryUsers = req.db.from("users").select("*").where("email", "=", email);
+  queryUsers.then(users => {
+    if (users.length > 0) {
+      console.log("User already exists");
+      return;
+    }
+
+    // Insert user into DB
+    const saltRounds = 10;
+    const hash = bcrypt.hashSync(password, saltRounds);
+    return req.db.from("users").insert({ email, hash });
+  })
+    .then(() => {
+      res.status(201).json({ success: true, message: "User created" });
+    });
+});
+
+module.exports = router;
