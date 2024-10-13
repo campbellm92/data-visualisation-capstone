1a6cc5f6b88389c259dce338bfe73e71747e15a9
Gary Cazzulino <gary@caz.email>
2024-10-13 12:47:52 +1000

    Added start and end query parameters to the /api/combined_data/{LGAName} end point to filter on date, and updated /docs

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,97 +1,135 @@
-var express = require("express"); 
-var router = express.Router(); 
+var express = require("express");
+var router = express.Router();
 const authorization = require("../middleware/authorization");
 
 
-/* GET home page. */ 
-router.get("/", function (req, res, next) { 
-  res.render("index", { title: "Express" }); 
-}); 
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
 
 
-router.get("/api/combined_data", function (req, res, next) { 
-  req.db 
-    .from("combined_data") 
-    .select("sample_date", "lga_name", "average_historical_occupancy", "average_daily_rate", "average_length_of_stay", "average_booking_window") 
-    .then((rows) => { 
-      res.json({ Error: false, Message: "Success", combined_data: rows }); 
-    }) 
-    .catch((err) => { 
-      console.log(err); 
-      res.json({ Error: true, Message: "Error in MySQL query" }); 
-    }); 
-}); 
+router.get("/api/combined_data", function (req, res, next) {
+  req.db
+    .from("combined_data")
+    .select("sample_date", "lga_name", "average_historical_occupancy", "average_daily_rate", "average_length_of_stay", "average_booking_window")
+    .then((rows) => {
+      res.json({ Error: false, Message: "Success", combined_data: rows });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({ Error: true, Message: "Error in MySQL query" });
+    });
+});
 
 
-router.get("/api/length_of_stay", function (req, res, next) { 
-  req.db 
-    .from("length_of_stay") 
-    .select("sample_date", "lga_name", "average_length_of_stay", "average_booking_window") 
-    .then((rows) => { 
-      res.json({ Error: false, Message: "Success", length_of_stay: rows }); 
-    }) 
-    .catch((err) => { 
-      console.log(err); 
-      res.json({ Error: true, Message: "Error in MySQL query" }); 
-    }); 
-}); 
+router.get("/api/length_of_stay", function (req, res, next) {
+  req.db
+    .from("length_of_stay")
+    .select("sample_date", "lga_name", "average_length_of_stay", "average_booking_window")
+    .then((rows) => {
+      res.json({ Error: false, Message: "Success", length_of_stay: rows });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({ Error: true, Message: "Error in MySQL query" });
+    });
+});
 
-router.get("/api/occupancy_daily_rate", function (req, res, next) { 
-  req.db 
-    .from("occupancy_daily_rate") 
-    .select("sample_date", "lga_name", "average_historical_occupancy", "average_daily_rate")
-    .then((rows) => { 
-      res.json({ Error: false, Message: "Success", occupancy_daily_rate: rows }); 
-    }) 
-    .catch((err) => { 
-      console.log(err); 
-      res.json({ Error: true, Message: "Error in MySQL query" }); 
-    }); 
-}); 
+router.get("/api/occupancy_daily_rate", function (req, res, next) {
+  req.db
+    .from("occupancy_daily_rate")
+    .select("sample_date", "lga_name", "average_historical_occupancy", "average_daily_rate")
+    .then((rows) => {
+      res.json({ Error: false, Message: "Success", occupancy_daily_rate: rows });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({ Error: true, Message: "Error in MySQL query" });
+    });
+});
 
-router.get("/api/combined_data/:LGAName", function (req, res, next) { 
-  req.db 
-    .from("combined_data") 
-    .select("sample_date", "lga_name", "average_historical_occupancy", "average_daily_rate", "average_length_of_stay", "average_booking_window") 
-    .where("lga_name", "=", req.params.LGAName) 
-    .then((rows) => { 
-      res.json({ Error: false, Message: "Success", combined_data: rows }); 
-    }) 
-    .catch((err) => { 
-      console.log(err); 
-      res.json({ Error: true, Message: "Error in MySQL query" }); 
-    }); 
-}); 
+router.get("/api/combined_data/:LGAName", function (req, res, next) {
+
+  console.log(`start[${req.query.start}] end[${req.query.end}]`)
+
+  if (req.query.start !== undefined && req.query.end !== undefined) {
+
+    req.db
+      .from("combined_data")
+      .select("sample_date", "lga_name", "average_historical_occupancy", "average_daily_rate", "average_length_of_stay", "average_booking_window")
+      .where("lga_name", "=", req.params.LGAName)
+      .where("sample_date", ">=", req.query.start)
+      .where("sample_date", "<=", req.query.end)
+      .then((rows) => {
+        res.json({ Error: false, Message: "Success", combined_data: rows });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json({ Error: true, Message: "Error in MySQL query " + err.sqlMessage});
+      });
+
+  } else if (req.query.start !== undefined) {
+
+    req.db
+      .from("combined_data")
+      .select("sample_date", "lga_name", "average_historical_occupancy", "average_daily_rate", "average_length_of_stay", "average_booking_window")
+      .where("lga_name", "=", req.params.LGAName)
+      .where("sample_date", ">=", req.query.start)
+      .then((rows) => {
+        res.json({ Error: false, Message: "Success", combined_data: rows });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json({ Error: true, Message: "Error in MySQL query " + err.sqlMessage });
+      });
+
+  } else {
+
+    req.db
+      .from("combined_data")
+      .select("sample_date", "lga_name", "average_historical_occupancy", "average_daily_rate", "average_length_of_stay", "average_booking_window")
+      .where("lga_name", "=", req.params.LGAName)
+      .then((rows) => {
+        res.json({ Error: false, Message: "Success", combined_data: rows });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json({ Error: true, Message: "Error in MySQL query " + err.sqlMessage });
+      });
+  }
+
+});
 
 
 /* original module examples below
 
-router.get("/api/city", function (req, res, next) { 
-  req.db 
-    .from("city") 
-    .select("name", "district") 
-    .then((rows) => { 
-      res.json({ Error: false, Message: "Success", City: rows }); 
-    }) 
-    .catch((err) => { 
-      console.log(err); 
-      res.json({ Error: true, Message: "Error in MySQL query" }); 
-    }); 
+router.get("/api/city", function (req, res, next) { 
+  req.db 
+    .from("city") 
+    .select("name", "district") 
+    .then((rows) => { 
+      res.json({ Error: false, Message: "Success", City: rows }); 
+    }) 
+    .catch((err) => { 
+      console.log(err); 
+      res.json({ Error: true, Message: "Error in MySQL query" }); 
+    }); 
 }); 
 
 
-router.get("/api/city/:CountryCode", function (req, res, next) { 
-  req.db 
-    .from("city") 
-    .select("*") 
-    .where("CountryCode", "=", req.params.CountryCode) 
-    .then((rows) => { 
-      res.json({ Error: false, Message: "Success", City: rows }); 
-    }) 
-    .catch((err) => { 
-      console.log(err); 
-      res.json({ Error: true, Message: "Error in MySQL query" }); 
-    }); 
+router.get("/api/city/:CountryCode", function (req, res, next) { 
+  req.db 
+    .from("city") 
+    .select("*") 
+    .where("CountryCode", "=", req.params.CountryCode) 
+    .then((rows) => { 
+      res.json({ Error: false, Message: "Success", City: rows }); 
+    }) 
+    .catch((err) => { 
+      console.log(err); 
+      res.json({ Error: true, Message: "Error in MySQL query" }); 
+    }); 
 }); 
 
 
@@ -114,5 +152,5 @@ router.post('/api/update', authorization, (req, res) => {
 */
 
 
-module.exports = router; 
+module.exports = router;
 
