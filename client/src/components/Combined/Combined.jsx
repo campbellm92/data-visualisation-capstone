290aafd7377af4d3cc10d410c36c05b45f26bd0b
Amrit Maharjan <amrit.mhrj@gmail.com>
2024-10-19 16:12:30 +1000

    Created CombinedComponent to display hero and diff as one on landing page

---
+++ client/src/components/Combined/Combined.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import HeroSection from "../sections/HeroSection";
+import Diff from "../layout/Diff";
+
+
+const CombinedComponent = () => {
+  return (
+    <div>
+      <HeroSection />
+      <Diff />
+    </div>
+  );
+};
+
+export default CombinedComponent;
