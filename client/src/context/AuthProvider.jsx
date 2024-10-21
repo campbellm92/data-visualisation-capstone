b6cc538f3f6861b31bebda8061d62df33c1ad385
Matthew Campbell <m.campbell92@gmail.com>
2024-10-21 18:45:23 +0200

    begin work on auth provider

---
+++ client/src/context/AuthProvider.jsx
@@ -0,0 +1,23 @@
+//on hold
+
+// import { createContext, useContext, useMemo, useState } from "react";
+
+// const authContext = createContext();
+
+// export default function AuthProvider({ children }) {
+//   const [token, setTokenState] = useState(localStorage.getItem("token"));
+//   const [isLoggedIn, setIsLoggedIn] = useState(!!token);
+
+//   const handleLogin
+// }
+
+// // const token = localStorage.getItem("token");
+// // const [isLoggedIn, setIsLoggedIn] = useState(
+// //   localStorage.getItem("token") ? true : false
+// // );
+
+// // if (token) {
+// //   console.log(`Token: ${token}, Logged in`);
+// // } else {
+// //   console.log("Token not found, not logged in");
+// // }
