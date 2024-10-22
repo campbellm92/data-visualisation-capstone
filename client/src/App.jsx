dd32a59216d73bdc3162c73bae99d2688b8ec53c
Matthew Campbell <m.campbell92@gmail.com>
2024-10-22 17:52:06 +0200

    import auth context logic, reconfig pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import { useContext } from "react";
+import { AuthContext, AuthProvider } from "./context/AuthProvider";
 import {
   BrowserRouter as Router,
   Routes,
@@ -6,73 +7,29 @@ import {
   Navigate,
 } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
-import CombinedComponent from "./components/Combined/Combined";
-import Welcome from "./components/Welcome/Welcome";
-import {
-  ButtonSmall,
-  ButtonMedium,
-  ButtonOutline,
-  ButtonSmallWide,
-} from "./components/ui/Buttons";
-import {
-  DropdownMonth,
-  DropdownSeason,
-  DropdownYear,
-} from "./components/ui/DropDowns";
-import {
-  TextInput,
-  SearchBar,
-  SearchBarWithOptional,
-  TextInputXS,
-  TextInputSM,
-  TextInputMD,
-  TextInputLG,
-} from "./components/ui/Inputs";
+import LandingPage from "./pages/LandingPage";
+import Welcome from "./pages/welcome/Welcome";
 
 function App() {
-  // State to check if user is logged in
-  // If token is present in local storage, user is logged in
-  const token = localStorage.getItem("token");
-  const [isLoggedIn, setIsLoggedIn] = useState(
-    localStorage.getItem("token") ? true : false
-  );
-
-  // Conditional logging based on token presence
-  if (token) {
-    console.log(`Token: ${token}, Logged in`);
-  } else {
-    console.log("Token not found, not logged in");
-  }
   return (
-    <Router>
-      <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
-      <Routes>
-        <Route path="/" element={<CombinedComponent />} />
-        <Route
-          path="/welcome"
-          element={isLoggedIn ? <Welcome /> : <Navigate to="/" />}
-        />
-      </Routes>
-      <ButtonSmall>Small</ButtonSmall>
-      <ButtonMedium>Medium</ButtonMedium>
-      <ButtonOutline>Outline</ButtonOutline>
-      <ButtonSmallWide>Wide</ButtonSmallWide>
-      <p className="text-info">Info</p>
-      <p className="text-success">Success</p>
-      <p className="text-warning">Warning</p>
-      <p className="text-error">Error</p>
-      <DropdownMonth />
-      <DropdownSeason />
-      <DropdownYear />
-      <TextInput />
-      <SearchBar />
-      <SearchBarWithOptional />
-      <TextInputXS />
-      <TextInputSM />
-      <TextInputMD />
-      <TextInputLG />
-    </Router>
+    <AuthProvider>
+      <Router>
+        {/* navbar renders everywhere */}
+        <Navbar />
+        <Routes>
+          {/* landing page code now in pages > LandingPage.jsx */}
+          <Route path="/" element={<LandingPage />} />
+          {/* This is now a protected route which uses the auth provider logic */}
+          <Route path="/welcome" element={<ProtectedRoute />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
+}
+
+function ProtectedRoute() {
+  const { isLoggedIn } = useContext(AuthContext);
+  return isLoggedIn ? <Welcome /> : <Navigate to="/" />;
 }
 
 export default App;
