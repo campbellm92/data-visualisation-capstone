8d18b7b33698c299b672e96aaa84e40d75f29d43
Matthew Campbell <m.campbell92@gmail.com>
2024-10-22 17:58:34 +0200

    move login state, toast, log out function to auth provider

--- client/src/context/AuthProvider.jsx
+++ client/src/context/AuthProvider.jsx
@@ -1,23 +1,40 @@
-//on hold
+import { createContext, useState, useEffect } from "react";
 
-// import { createContext, useContext, useMemo, useState } from "react";
+export const AuthContext = createContext();
 
-// const authContext = createContext();
+export function AuthProvider({ children }) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // state for showing logout Toast component
+  const [showLogoutToast, setShowLogoutToast] = useState(false);
 
-// export default function AuthProvider({ children }) {
-//   const [token, setTokenState] = useState(localStorage.getItem("token"));
-//   const [isLoggedIn, setIsLoggedIn] = useState(!!token);
+  // check if user is already logged in
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setIsLoggedIn(true);
+      console.log(`Token: ${token}, Logged in`);
+      console.log("Token not found, not logged in");
+    }
+  }, []);
 
-//   const handleLogin
-// }
-
-// // const token = localStorage.getItem("token");
-// // const [isLoggedIn, setIsLoggedIn] = useState(
-// //   localStorage.getItem("token") ? true : false
-// // );
-
-// // if (token) {
-// //   console.log(`Token: ${token}, Logged in`);
-// // } else {
-// //   console.log("Token not found, not logged in");
-// // }
+  // function for handling logout and showing the toast
+  const handleLogout = () => {
+    setTimeout(() => {
+      localStorage.removeItem("token");
+      setIsLoggedIn(false);
+      setShowLogoutToast(true);
+      console.log("Token removed");
+      console.log("Logged out");
+      setTimeout(() => {
+        setShowLogoutToast(false); // hide toast after 3 seconds
+      }, 3000);
+    }, 1500);
+  };
+  return (
+    <AuthContext.Provider
+      value={{ isLoggedIn, setIsLoggedIn, handleLogout, showLogoutToast }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
