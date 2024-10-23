8368940b0504d86803f7fd623ed113b31b532eb3
Matthew Campbell <m.campbell92@gmail.com>
2024-10-23 11:36:12 +0200

    change hero

--- client/src/sections/landing-page/HeroSection.jsx
+++ client/src/sections/landing-page/HeroSection.jsx
@@ -3,16 +3,18 @@ import { ButtonMediumWide } from "../../components/ui/Buttons";
 export default function HeroSection() {
   return (
     <div>
-      <div className="hero bg-base-300 min-h-screen">
-        <div className="hero-content text-primary-content text-center z-auto">
-          <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Hello there</h1>
+      <div className="hero bg-base-300 min-h-screen -mt-10 z-auto">
+        <div className="hero-content text-primary-content text-center">
+          <div className="max-w-3xl">
+            <h1 className="text-6xl sm:text-5xl md:text-7xl lg:text-8xl xl:text-9xl font-bold">
+              Data-driven insights for smart tourism
+            </h1>
             <p className="py-6">
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
               excepturi exercitationem quasi. In deleniti eaque aut repudiandae
               et a id nisi.
             </p>
-            <ButtonMediumWide>Hey</ButtonMediumWide>{" "}
+            <ButtonMediumWide>Get to know us</ButtonMediumWide>{" "}
           </div>
         </div>
       </div>
