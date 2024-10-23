92f7a811d5871f9c50352ba2bd01831022d696e8
Matthew Campbell <m.campbell92@gmail.com>
2024-10-23 15:09:55 +0200

    construct info section

---
+++ client/src/sections/landing-page/InfoSection.jsx
@@ -0,0 +1,40 @@
+export default function InfoSection() {
+  return (
+    <div className="h-screen">
+      <div className="info-part bg-base-100 flex flex-col lg:flex-row-reverse items-center justify-center w-full">
+        <div className="hero w-full h-full">
+          <div className="hero-content flex-col lg:flex-row-reverse text-center justify-between lg:p-10">
+            <img
+              src="/assets/images/josh-withers-9h479w-syaQ-unsplash.jpg"
+              className="max-w-xs md:max-w-sm lg:max-w-md rounded-lg shadow-2xl object-cover sm:mt-6"
+            />
+            <div className="lg:mr-10 sm:my-6">
+              <h1 className="text-5xl font-bold">
+                Hey! 👋 We're{" "}
+                <span className="text-bold text-primary underline decoration-base-200">
+                  Localis
+                </span>
+              </h1>
+              <p className="py-6">
+                We provide lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Eius natus nemo quis! Quos, numquam sed tenetur,
+                dignissimos nesciunt saepe iste eum incidunt similique deserunt
+                cupiditate. Magnam facilis error ut fugit!
+              </p>
+              <button className="btn btn-primary">Get Started</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="client-logos-part bg-base-100 flex-grow">
+        <div className="header bg-primary-content text-primary text-xl font-bold text-center pt-10">
+          WHO WE'VE HELPED
+        </div>
+        <div className="banner">
+          <img src="/assets/logos/companies-logos.png" alt="Company logos" />
+        </div>
+      </div>
+    </div>
+  );
+}
