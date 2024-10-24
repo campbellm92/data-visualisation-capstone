291ebfc4d7e5ef22462fdc7e8b5c818ec5e7e48d
Matthew Campbell <m.campbell92@gmail.com>
2024-10-24 16:02:03 +0200

    impost error msg const

--- client/src/api/hooks/useLocalisData.jsx
+++ client/src/api/hooks/useLocalisData.jsx
@@ -1,18 +1,17 @@
-// 
+//
 //  IFQ717 Web Development Capstone
 //
 //  api.js - Main app API function definitions
 //
 //
 
-import { useState, useEffect } from 'react';
-import { addDaysToDate } from '../../utils/utils';
-import { kAPI_URL } from '../../utils/constants';
-import { getUrlFromCache, getUrlFromServer } from '../../utils/utils';
+import { useState, useEffect } from "react";
+import { addDaysToDate } from "../../utils/utils";
+import { kAPI_URL, kDEFAULT_ERROR_MSG } from "../../utils/constants";
+import { getUrlFromCache, getUrlFromServer } from "../../utils/utils";
 
 // Retrieve Ranking data based on year and country from the external API
 export function useLocalisData(startDate, windowDays) {
-
   const [loading, setLoading] = useState(false);
   const [dataSet, setDataSet] = useState();
   const [error, setError] = useState(null);
@@ -23,36 +22,36 @@ export function useLocalisData(startDate, windowDays) {
   // console.log(startDateDate);
   // console.log(endDateDate);
 
-
   useEffect(() => {
-
     let url = `${kAPI_URL}/combined_data`; //?start=${startDate}&end=${addDaysToDate(startDate, windowDays)}`;
 
     if (getUrlFromCache(cache, url)) {
-
       setLoading(false);
-      setDataSet(getUrlFromCache(cache, url).filter(sample => {
-         const sampleDateDate = new Date(sample.sample_date);
+      setDataSet(
+        getUrlFromCache(cache, url).filter((sample) => {
+          const sampleDateDate = new Date(sample.sample_date);
 
-         return (sampleDateDate >= startDateDate && sampleDateDate <= endDateDate); 
-
-        }));
+          return (
+            sampleDateDate >= startDateDate && sampleDateDate <= endDateDate
+          );
+        })
+      );
       setError(null);
-
     } else {
-
       setLoading(true);
       getUrlFromServer(url, false)
         .then((dataSet) => {
-          setCache((prevCache) => ({ ...prevCache,  [url]: dataSet }));
-          setDataSet(getUrlFromCache(cache, url).filter(sample => {
-         const sampleDateDate = new Date(sample.sample_date);
+          setCache((prevCache) => ({ ...prevCache, [url]: dataSet }));
+          setDataSet(
+            getUrlFromCache(cache, url).filter((sample) => {
+              const sampleDateDate = new Date(sample.sample_date);
 
-         return (sampleDateDate >= startDateDate && sampleDateDate <= endDateDate); 
-         
-        }));
+              return (
+                sampleDateDate >= startDateDate && sampleDateDate <= endDateDate
+              );
+            })
+          );
           //setDataSet(dataSet);
-
         })
         .catch((error) => {
           setError(error.message ?? kDEFAULT_ERROR_MSG);
@@ -66,7 +65,6 @@ export function useLocalisData(startDate, windowDays) {
   return {
     loading: loading,
     dataSet: dataSet,
-    error: error
+    error: error,
   };
-
 }
