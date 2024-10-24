74beaac85a93a499f5c3050bed82a06154f43fc8
Matthew Campbell <m.campbell92@gmail.com>
2024-10-24 17:30:45 +0200

    create fetch hook w minimal params

---
+++ client/src/api/hooks/useFetchLocalisData.js
@@ -0,0 +1,48 @@
+// a more generalist fetch hook
+
+import { useState, useEffect } from "react";
+import { kAPI_URL, kDEFAULT_ERROR_MSG } from "../utils/constants";
+import { getUrlFromCache, getUrlFromServer } from "../utils/utils";
+
+export default function useFetchLocalisData(endpoint) {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState();
+  const [error, setError] = useState(null);
+  const [cache, setCache] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+
+      let url = `${kAPI_URL}${endpoint}`;
+
+      const cachedData = getUrlFromCache(cache, url);
+
+      if (cachedData) {
+        setLoading(false);
+        setData(cachedData);
+        setError(null);
+      } else {
+        setLoading(true);
+        try {
+          const dataSet = await getUrlFromServer(url, false);
+          setCache((prevCache) => ({ ...prevCache, [url]: dataSet }));
+          setData(dataSet);
+          setError(null);
+        } catch (error) {
+          setError(error.message ?? kDEFAULT_ERROR_MSG);
+        } finally {
+          setLoading(false);
+        }
+      }
+    };
+
+    fetchData();
+  });
+
+  return {
+    loading,
+    data,
+    error,
+  };
+}
