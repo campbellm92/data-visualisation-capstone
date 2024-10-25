328283a42cca7e0209d75ac1ee76d8cec8e2d6d2
Matthew Campbell <m.campbell92@gmail.com>
2024-10-25 13:09:58 +0200

    introduce helper func logic

--- client/src/components/charts/treemaps/ADRTreeMap.jsx
+++ client/src/components/charts/treemaps/ADRTreeMap.jsx
@@ -1,17 +1,25 @@
 import { Treemap, ResponsiveContainer } from "recharts";
-import { useLocalisData } from "../../../api/hooks/useLocalisData";
+import { useFetchLocalisData } from "../../../api/hooks/useFetchLocalisData";
+import { filterDataByLGA } from "../../../api/utils/filterUtils";
+import { getMonthlyAverageADR } from "../../../api/utils/getUtils";
 
 // a tree map representing averages by month for ADR - Average daily rate
 
-export default function ADRTreeMap() {
-  const { loading, dataSet, error } = useLocalisData();
+export default function ADRTreeMap({ lgaName, year }) {
+  const { loading, data, error } = useFetchLocalisData("/combined_data");
+
+  const filteredDataByLGA = data ? filterDataByLGA(data, lgaName) : null;
+
+  const monthlyAverageData = getMonthlyAverageADR(filteredDataByLGA, year);
+  console.log(monthlyAverageData);
+
   return (
     <ResponsiveContainer width="100%" height="100%">
       <Treemap
         width={400}
         height={200}
-        data={dataSet}
-        dataKey="size"
+        data={monthlyAverageData}
+        dataKey="value"
         aspectRatio={4 / 3}
         stroke="#fff"
         fill="#8884d8"
@@ -19,3 +27,20 @@ export default function ADRTreeMap() {
     </ResponsiveContainer>
   );
 }
+
+// steps:
+
+// 1. Function for converting numeric date to string (CHECK)
+// 2. Function for getting the average ADR figure per month
+// 3. Function for isolating LGA_name (CHECK)
+
+/*
+
+What I want/need to do:
+I want to have the dates converted to strings so they can be represented as such.
+I need to group the data by month. Is this not already done with the dateToString func? 
+-- perhaps not because then I'd need to hard code if statements for getting month.. if (month === "January") ... 
+The numeric data to str function ONLY converts to string. 
+
+
+*/
