2e94c10bacd748a590c56b0bf8fa9efb81e01200
Matthew Campbell <m.campbell92@gmail.com>
2024-10-25 13:10:07 +0200

    add props for testing

--- client/src/pages/data-visualisation/LGAPage.jsx
+++ client/src/pages/data-visualisation/LGAPage.jsx
@@ -1,5 +1,9 @@
 import ADRTreeMap from "../../components/charts/treemaps/ADRTreeMap";
 
 export default function LGAPage() {
-  return <ADRTreeMap />;
+  return (
+    <div style={{ width: "400px", height: "400px" }}>
+      <ADRTreeMap lgaName={"Whitsunday"} year={2024} />;
+    </div>
+  );
 }
