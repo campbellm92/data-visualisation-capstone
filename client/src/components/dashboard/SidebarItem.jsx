d536adcf771e17c6a5a94830bc2b845b9dc0494d
Matthew Campbell <m.campbell92@gmail.com>
2024-10-29 16:35:24 +0100

    set up logic for sidebar items

---
+++ client/src/components/dashboard/SidebarItem.jsx
@@ -0,0 +1,26 @@
+import { Link, useLocation } from "react-router-dom";
+
+export default function SidebarItem({ href, label, isCollapsed, icon }) {
+  const location = useLocation();
+  const isActive = location.pathname === href;
+
+  return (
+    <li className={`group`}>
+      <Link
+        to={href}
+        className={`flex items-center py-3 px-4 hover:bg-base-200 transition ${
+          isActive ? "bg-base-300" : ""
+        }`}
+      >
+        {icon}
+        <span
+          className={`text-primary-content ml-4 ${
+            isCollapsed ? "hidden" : "block"
+          }`}
+        >
+          {label}
+        </span>
+      </Link>
+    </li>
+  );
+}
