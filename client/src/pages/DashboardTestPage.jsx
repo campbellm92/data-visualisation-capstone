d89a3fcc50901e71cc2a140812c971a771feefec
Matthew Campbell <m.campbell92@gmail.com>
2024-10-29 16:38:06 +0100

    add dashboard test page for testing ui

---
+++ client/src/pages/DashboardTestPage.jsx
@@ -0,0 +1,15 @@
+export default function DashboardTestPage() {
+  return (
+    <div>
+      <div className="w-20 h-20 border bg-primary"></div>
+      <div className="w-20 h-20 border bg-secondary"></div>
+      <div className="w-20 h-20 border bg-base-100"></div>
+      <div className="w-20 h-20 border bg-base-200"></div>
+      <div className="w-20 h-20 border bg-base-300"></div>
+      <div className="w-20 h-20 border bg-info"></div>
+      <div className="w-20 h-20 border bg-success"></div>
+      <div className="w-20 h-20 border bg-warning"></div>
+      <div className="w-20 h-20 border bg-error"></div>
+    </div>
+  );
+}
