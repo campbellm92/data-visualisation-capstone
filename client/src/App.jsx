0cf4ba4f01e6072a6faaa12f0719fc34eb92c5fc
Matthew Campbell <m.campbell92@gmail.com>
2024-10-29 16:32:10 +0100

    import layout logic for public vs private endpoints / dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,71 @@
 import { useContext } from "react";
-import { AuthContext, AuthProvider } from "./context/AuthProvider";
+import { AuthContext } from "./context/AuthProvider";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
 } from "react-router-dom";
-import Navbar from "./components/layout/Navbar";
+import PublicLayout from "./components/layout/PublicLayout";
+import DashboardLayout from "./components/layout/DashboardLayout";
 import LandingPage from "./pages/LandingPage";
 import Welcome from "./pages/welcome/Welcome";
 import LGAPage from "./pages/data-visualisation/LGAPage";
-import Footer from "./sections/footer/Footer";
+import DashboardTestPage from "./pages/DashboardTestPage"; // delete when finished
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        {/* navbar renders everywhere */}
-        <Navbar />
-        <Routes>
-          {/* landing page code now in pages > LandingPage.jsx */}
-          <Route path="/" element={<LandingPage />} />
-          {/* This is now a protected route which uses the auth provider logic */}
-          <Route
-            path="/welcome"
-            element={<ProtectedRoute component={Welcome} />}
-          />
-          <Route
-            path="/lgapage"
-            element={<ProtectedRoute component={LGAPage} />}
-          />
-        </Routes>
-        <Footer />
-      </Router>
-    </AuthProvider>
+    <Router>
+      {/* navbar renders everywhere */}
+      {/* <Navbar /> */}
+      <Routes>
+        {/* landing page code now in pages > LandingPage.jsx */}
+        <Route
+          path="/"
+          element={
+            <PublicLayout>
+              <LandingPage />
+            </PublicLayout>
+          }
+        />
+        <Route
+          path="/welcome"
+          element={
+            <ProtectedRoute>
+              <DashboardLayout>
+                <Welcome />
+              </DashboardLayout>
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/lgapage"
+          element={
+            <ProtectedRoute>
+              <DashboardLayout>
+                <LGAPage />
+              </DashboardLayout>
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboardtest"
+          element={
+            <ProtectedRoute>
+              <DashboardLayout>
+                <DashboardTestPage />
+              </DashboardLayout>
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
-function ProtectedRoute({ component: Component }) {
+function ProtectedRoute({ children }) {
   const { isLoggedIn } = useContext(AuthContext);
-  return isLoggedIn ? <Component /> : <Navigate to="/" />;
+  return isLoggedIn ? children : <Navigate to="/" />;
 }
 
 export default App;
