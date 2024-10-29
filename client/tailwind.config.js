5b878023a8217f02376f6096ddf4467cc6fc74b3
Matthew Campbell <m.campbell92@gmail.com>
2024-10-29 16:31:26 +0100

    begin configuring other themes, dark theme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,20 +9,46 @@ export default {
   daisyui: {
     themes: [
       {
-        mytheme: {
+        noosa: {
           primary: "#c88441", // buttons
-          "primary-content": "#edebe7", // text
-          // secondary: "",
+          "primary-content": "#0f172a", // text
+          secondary: "#0ea5e9",
           // "secondary-content": "",
           // accent: "",
           // "accent-content": "",
-          "base-100": "#ffffff", // background
-          // "base-200": "#7a8e7c",
-          "base-200": "#0ea5e9",
-          // "base-300": "#173a2c",
-          // "base-300": "#0c4a6e",
-          // "base-300": "#111827",
-          "base-300": "#1f2937",
+          "base-100": "#4f46e5",
+          "base-200": "#818cf8",
+          "base-300": "#eef2ff",
+          info: "#237496",
+          success: "#289b6f",
+          warning: "#fccd5e",
+          error: "#f87979",
+        },
+      },
+      {
+        gold_coast: {},
+      },
+      {
+        whitsunday: {},
+      },
+      {
+        cairns: {},
+      },
+      {
+        dark: {
+          primary: "#c88441", // buttons
+          "primary-content": "#edebe7", // text
+          secondary: "#0ea5e9",
+          // "secondary-content": "",
+          // accent: "",
+          // "accent-content": "",
+          // "base-100": "#ffffff",
+          "base-100": "#374151",
+          // "base-200": "#374151",
+          "base-200": "#1f2937",
+          // "base-300": "#1f2937",
+          "base-300": "#111827",
+          // base 300 change to #111827?
 
           info: "#237496",
           success: "#289b6f",
