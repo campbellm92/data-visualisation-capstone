ac149f248ebe5839e7c83e6a9b5fd99cb43f11ba
Matthew Campbell <m.campbell92@gmail.com>
2024-10-31 20:05:51 +0100

    create fetch user data hook

---
+++ client/src/api/hooks/useFetchUserData.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import { getUrlFromCache, getUrlFromServer } from "../utils/utils";
+
+export function useFetchUserData(endpoint) {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState();
+  const [error, setError] = useState(null);
+  const [cache, setCache] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+
+      let url = `http://localhost:3000${endpoint}`;
+
+      const cachedData = getUrlFromCache(cache, url);
+
+      if (cachedData) {
+        setLoading(false);
+        setData(cachedData);
+        setError(null);
+      } else {
+        try {
+          const dataSet = await getUrlFromServer(url, false);
+          setCache((prevCache) => ({ ...prevCache, [url]: dataSet }));
+          setData(dataSet);
+          setError(null);
+        } catch (error) {
+          setError(error.message);
+        } finally {
+          setLoading(false);
+        }
+      }
+    };
+    fetchData();
+  }, [endpoint]);
+
+  return {
+    loading,
+    data,
+    error,
+  };
+}
