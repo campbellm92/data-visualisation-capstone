4cd683fb23f2e13273aa4122ef9b00dac06d4944
Matthew Campbell <m.campbell92@gmail.com>
2024-11-09 15:32:43 +0100

    add fields constants, map to charts

--- client/src/pages/DashboardTestPage.jsx
+++ client/src/pages/DashboardTestPage.jsx
@@ -1,18 +1,27 @@
 import BarChartHome from "../components/charts/barcharts/BarChartHome";
 
 export default function DashboardTestPage() {
+  const fields = [
+    "average_daily_rate",
+    "average_booking_window",
+    "average_historical_occupancy",
+    "average_length_of_stay",
+  ];
+
   return (
     <div>
-      <BarChartHome
-        lgaName={"Cairns"}
-        year={2023}
-        dataField={"average_daily_rate"}
-      />
-      <BarChartHome
-        lgaName={"Whitsunday"}
-        year={2023}
-        dataField={"average_booking_window"}
-      />
+      <div className="pb-10">
+        <h1 className="text-xl">
+          Overview for <span className="text-primary">Whitsunday</span>
+        </h1>
+      </div>
+      <div className="grid grid-cols-2 gap-8">
+        {fields.map((field) => (
+          <div key={field}>
+            <BarChartHome year={2023} dataField={field} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
