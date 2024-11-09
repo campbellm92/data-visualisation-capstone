862a9d3537b0e27cb4a2682be3cfcc603b5947b9
Matthew Campbell <m.campbell92@gmail.com>
2024-11-09 10:32:12 +0100

    import different btn, text & bg styles

--- client/src/components/layout/AnalyseModal.jsx
+++ client/src/components/layout/AnalyseModal.jsx
@@ -6,122 +6,147 @@
 //
 
 import { useState } from "react";
-import { ButtonOutlineFullWide } from "../ui/Buttons";
+import { ButtonMediumFullWide } from "../ui/Buttons";
 import { kAPI_URL } from "../../api/utils/constants";
 import LoadingSpinner from "../ui/LoadingSpinner";
-import LLMResponse from './LLMResponse';
+import LLMResponse from "./LLMResponse";
 import { SelectLLMPrompt } from "../ui/Select";
 
-const kDefaultResponse = 'Hi there, please select a default prompt or enter a custom prompt above and then click Analyse to pass your prompt and the selected data to an LLM for analysis.';
+const kDefaultResponse =
+  "Hi there, please select a default prompt or enter a custom prompt above and then click Analyse to pass your prompt and the selected data to an LLM for analysis.";
 
 function handleSelectChange() {
-    document.getElementById('prompt').value = '';
-
+  document.getElementById("prompt").value = "";
 }
 
 const AnalyseModal = ({ dataSet }) => {
+  const [llmResponse, setllmResponse] = useState(kDefaultResponse);
 
-    const [llmResponse, setllmResponse] = useState(kDefaultResponse);
+  function closeForm() {
+    setllmResponse(kDefaultResponse);
+    document.getElementById("analyse_modal").close();
+  }
 
-    function closeForm() {
-        setllmResponse(kDefaultResponse);
-        document.getElementById("analyse_modal").close();
+  async function doAnalysis(e) {
+    const url = `${kAPI_URL}/ai/query_llm`;
+    const working = document.getElementById("working");
+    const analyseButton = document.getElementById("analyse-button");
+    const customPromptField = document.getElementById("prompt");
+    const defaultPromptsField = document.getElementById("defaultLLMPrompt");
+
+    setllmResponse("");
+    analyseButton.disabled = true;
+    working.style = "display:block";
+    e.preventDefault();
+    e.stopPropagation();
+
+    let promptField = defaultPromptsField;
+
+    if (customPromptField.value.length > 0) promptField = customPromptField;
+
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          prompt: promptField.value + " " + JSON.stringify(dataSet),
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+
+      if (response.error) {
+        alert(response.message);
+      } else {
+        console.log("Success:", data);
+      }
+
+      setllmResponse(data.response);
+      working.style = "display:none";
+      analyseButton.disabled = false;
+    } catch (error) {
+      setllmResponse(
+        "Too much data selected. Please select less data and try again. Version 2 will fix this issue."
+      );
+      working.style = "display:none";
+      analyseButton.disabled = false;
     }
+  }
 
-    async function doAnalysis(e) {
-
-        const url = `${kAPI_URL}/ai/query_llm`;
-        const working = document.getElementById('working');
-        const analyseButton = document.getElementById('analyse-button');
-        const customPromptField = document.getElementById('prompt');
-        const defaultPromptsField = document.getElementById('defaultLLMPrompt');
-
-        setllmResponse('');
-        analyseButton.disabled = true;
-        working.style = 'display:block';
-        e.preventDefault(); e.stopPropagation();
-
-        let promptField = defaultPromptsField;
-
-        if (customPromptField.value.length > 0) promptField = customPromptField;
-
-        try {
-            const response = await fetch(url, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ "prompt": promptField.value + ' ' + JSON.stringify(dataSet) }),
-            });
-
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-
-            const data = await response.json();
-
-            if (response.error) {
-                alert(response.message);
-            }
-            else {
-                console.log("Success:", data);
-            }
-
-            setllmResponse(data.response);
-            working.style = 'display:none';
-            analyseButton.disabled = false;
-
-        } catch (error) {
-            setllmResponse('Too much data selected. Please select less data and try again. Version 2 will fix this issue.');
-            working.style = 'display:none';
-            analyseButton.disabled = false;
-        }
-    }
-
-    return (
-        <dialog id="analyse_modal" className="modal w-full">
-            <div className="modal-box relative">
-                <button
-                    className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
-                    onClick={() => closeForm()}
-                >
-                    ✕
-                </button>
-                <form id="prompt-form" method="dialog" className="modal-backdrop">
-                    <br></br>
-                    <label id="prompt-label" className="form-control text-base-content">
-                        <div className="label">
-                            <span className="label-text">Use a Default Prompt</span>
-                        </div>
-                        <SelectLLMPrompt id="defaultLLMPrompt" handleSelectChange={handleSelectChange} />
-                    </label>
-                    <label id="prompt-label" className="form-control text-base-content">
-                        <div className="label">
-                            <span className="label-text">Or Write a Custom Prompt</span>
-                        </div>
-                        <textarea id="prompt" className="textarea textarea-bordered h-24 text-base-content box-drop-shadow" placeholder="" style={{maxHeight: '40vh' }}></textarea>
-                    </label>
-                </form>
-
-                <label id="response-label" className="form-control mb-5">
-                    <div className="label">
-                        <span className="label-text">LLM Response</span>
-                    </div>
-                    <div className='box-drop-shadow overflow-scroll' style={{ minHeight: '30vh', maxHeight: '40vh' }}>
-                        <LLMResponse content={llmResponse} />
-                        <div className="text-left" id="working" style={{ display: 'none' }}>
-                            <LoadingSpinner />
-                        </div>
-                    </div>
-                </label>
-                <div>
-
-                </div>
-                <ButtonOutlineFullWide id="analyse-button" children="Analyse" onClick={(e) => { doAnalysis(e); return false; }} />
+  return (
+    <dialog id="analyse_modal" className="modal w-full">
+      <div className="modal-box relative shadow-lg bg-base-300">
+        <button
+          className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+          onClick={() => closeForm()}
+        >
+          ✕
+        </button>
+        <form id="prompt-form" method="dialog">
+          <br></br>
+          <label
+            id="prompt-label"
+            className="form-control text-primary-content"
+          >
+            <div className="label">
+              <span className="text-primary-content">Use a Default Prompt</span>
             </div>
+            <SelectLLMPrompt
+              id="defaultLLMPrompt"
+              handleSelectChange={handleSelectChange}
+            />
+          </label>
+          <label
+            id="prompt-label"
+            className="form-control text-primary-content"
+          >
+            <div className="label">
+              <span className="text-primary-content">
+                Or Write a Custom Prompt
+              </span>
+            </div>
+            <textarea
+              id="prompt"
+              className="textarea textarea-bordered h-24 text-primary-content shadow-lg bg-base-300"
+              placeholder=""
+              style={{ maxHeight: "40vh" }}
+            ></textarea>
+          </label>
+        </form>
 
-        </dialog>
-    );
+        <label id="response-label" className="form-control mb-5">
+          <div className="label">
+            <span className="text-primary-content">LLM Response</span>
+          </div>
+          <div
+            className="shadow-lg overflow-scroll bg-base-300"
+            style={{ minHeight: "30vh", maxHeight: "40vh" }}
+          >
+            <LLMResponse content={llmResponse} />
+            <div className="text-left" id="working" style={{ display: "none" }}>
+              <LoadingSpinner />
+            </div>
+          </div>
+        </label>
+        <div></div>
+        <ButtonMediumFullWide
+          id="analyse-button"
+          onClick={(e) => {
+            doAnalysis(e);
+            return false;
+          }}
+        >
+          Analyse
+        </ButtonMediumFullWide>
+      </div>
+    </dialog>
+  );
 };
 
 export default AnalyseModal;
