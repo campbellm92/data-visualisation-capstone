f6254e20ea5ca69ca4afeae2185deb6ae0909c5b
Matthew Campbell <m.campbell92@gmail.com>
2024-11-09 10:34:52 +0100

    add font light

--- client/src/components/ui/Checkbox.jsx
+++ client/src/components/ui/Checkbox.jsx
@@ -1,19 +1,24 @@
-
-
 export default function Checkbox({ label, value, setValue }) {
+  const handleChange = () => {
+    setValue(!value);
+  };
 
-    const handleChange = () => {
-        setValue(!value);
-    };
-
-    return (
-        <div className="grid grid-cols-5 mt-1 ml-4">
-            <div className="p-0 col-span-1">
-                <input id={label} type="checkbox" checked={value} className="checkbox checkbox-xl" onChange={handleChange} />
-            </div>
-            <div className="p-0 col-span-4">
-                <label className="whitespace-nowrap text-primary-content">{label}</label>
-            </div>
-        </div>
-    )
+  return (
+    <div className="grid grid-cols-5 m-4">
+      <div className="p-0 col-span-1">
+        <input
+          id={label}
+          type="checkbox"
+          checked={value}
+          className="checkbox checkbox-xl"
+          onChange={handleChange}
+        />
+      </div>
+      <div className="pl-2 col-span-4">
+        <label className="whitespace-nowrap text-primary-content font-light">
+          {label}
+        </label>
+      </div>
+    </div>
+  );
 }
