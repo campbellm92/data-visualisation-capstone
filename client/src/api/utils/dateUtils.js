8d7faf8e0b620f003d7be6eb47459a4795fa602b
Matthew Campbell <m.campbell92@gmail.com>
2024-11-09 10:24:30 +0100

    add function for converting months to seasons

--- client/src/api/utils/dateUtils.js
+++ client/src/api/utils/dateUtils.js
@@ -1,4 +1,4 @@
-// convert dates in YYYY-MM-DD format to Month and year string
+// convert dates in YYYY-MM-DD format to month and year string (e.g. January 2023)
 export function numericDateToString(date) {
   const dateToConvert = new Date(date);
 
@@ -10,5 +10,29 @@ export function numericDateToString(date) {
 
   return { month, year };
 }
+//output: January 2023
 
-// will return (for example) January 2023
+// convert numeric dates to season names (Summer, Autumn, etc)
+export function convertMonthsToSeasons(month, year) {
+  switch (month) {
+    case "December":
+      return `Summer ${year}`;
+    case "January":
+    case "February":
+      return `Summer ${year}`;
+    case "March":
+    case "April":
+    case "May":
+      return `Autumn ${year}`;
+    case "June":
+    case "July":
+    case "August":
+      return `Winter ${year}`;
+    case "September":
+    case "October":
+    case "November":
+      return `Spring ${year}`;
+    default:
+      return null;
+  }
+}
