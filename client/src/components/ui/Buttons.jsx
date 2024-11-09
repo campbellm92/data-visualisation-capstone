b32117510ab9985930169849c3ee2ee517a4eb61
Matthew Campbell <m.campbell92@gmail.com>
2024-11-09 10:34:33 +0100

    adjust font & bg classes

--- client/src/components/ui/Buttons.jsx
+++ client/src/components/ui/Buttons.jsx
@@ -1,6 +1,9 @@
 export function ButtonSmall({ children, onClick }) {
   return (
-    <button className="btn btn-sm btn-primary text-primary-content" onClick={onClick}>
+    <button
+      className="btn btn-sm btn-primary text-primary-content"
+      onClick={onClick}
+    >
       {children}
     </button>
   );
@@ -30,20 +33,31 @@ export function ButtonSmallWide({ children }) {
 
 export function ButtonMediumWide({ children, onClick }) {
   return (
-    <button className="btn btn-md btn-primary btn-wide" onClick={onClick}>{children}</button>
+    <button className="btn btn-md btn-primary btn-wide" onClick={onClick}>
+      {children}
+    </button>
   );
 }
 
 export function ButtonMediumFullWide({ children, onClick }) {
   return (
-    <button className="btn btn-md btn-primary btn-wide w-full" onClick={onClick}>{children}</button>
+    <button
+      className="btn btn-md btn-primary btn-wide w-full text-secondary-content"
+      onClick={onClick}
+    >
+      {children}
+    </button>
   );
 }
-
 
 export function ButtonOutlineFullWide({ children, onClick, id }) {
   return (
-    <button id={id} className="btn btn-md btn-outline btn-wide w-full" onClick={onClick}>{children}</button>
+    <button
+      id={id}
+      className="btn btn-md btn-outline btn-wide w-full"
+      onClick={onClick}
+    >
+      {children}
+    </button>
   );
 }
-
