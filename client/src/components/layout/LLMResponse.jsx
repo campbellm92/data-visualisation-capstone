16710dba48cd40bb0ca71d6f60708b21d85f555a
Matthew Campbell <m.campbell92@gmail.com>
2024-11-09 10:33:31 +0100

    es linting

--- client/src/components/layout/LLMResponse.jsx
+++ client/src/components/layout/LLMResponse.jsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 const LLMResponse = ({ content }) => {
-    return (
-        <div className='markdown-content'>
-            <ReactMarkdown remarkPlugins={[remarkGfm]}>
-                {content}
-            </ReactMarkdown>
-        </div>
-    );
+  return (
+    <div className="markdown-content">
+      <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+    </div>
+  );
 };
 
 export default LLMResponse;
