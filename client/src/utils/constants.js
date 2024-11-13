37bd783c6f239939043d5c6343d404fc83f9102a
Matthew Campbell <m.campbell92@gmail.com>
2024-11-13 16:25:28 +0100

    move constants to separate file

---
+++ client/src/utils/constants.js
@@ -0,0 +1,24 @@
+// bare data fields for use within components (see )
+export const fields = [
+  "average_daily_rate",
+  "average_booking_window",
+  "average_historical_occupancy",
+  "average_length_of_stay",
+];
+
+// convert database field names to display names that are better for displaying the data
+export const dataFieldDisplayNames = {
+  average_booking_window: "Average Booking Window",
+  average_daily_rate: "Average Daily Rate",
+  average_historical_occupancy: "Average Historical Occupancy",
+  average_length_of_stay: "Average Length of Stay",
+};
+
+// colour scheme for the graphs for different LGAs
+// note: because of Recharts restrictions, this cannot be handled in tailwind.config.js
+export const lgaColours = {
+  Cairns: { light: "#15803d", dark: "#c88441" },
+  Noosa: { light: "#7e22ce", dark: "#c88441" },
+  Whitsunday: { light: "#0e7490", dark: "#c88441" },
+  "Gold Coast": { light: "#c88441", dark: "#c88441" },
+};
