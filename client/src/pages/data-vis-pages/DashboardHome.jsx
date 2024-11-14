0660ae6ec281ebe9d1370dcf3b779457e92b134a
Matthew Campbell <m.campbell92@gmail.com>
2024-11-14 12:28:03 +0100

    remove comments

--- client/src/pages/data-vis-pages/DashboardHome.jsx
+++ client/src/pages/data-vis-pages/DashboardHome.jsx
@@ -8,14 +8,11 @@ export default function DashboardHome() {
 
   return (
     <div>
-      {/* Page Header */}
       <div className="pb-5">
         <h1 className="font-light text-xl">
           Overview for <span className="text-primary">{userLGA}</span>
         </h1>
       </div>
-
-      {/* BarChart Section */}
       <DashboardBarChartsSection />
     </div>
   );
