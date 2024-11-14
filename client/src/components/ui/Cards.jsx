99ee27be69f79817d3e62ac5e3b15cdc317e3852
Matthew Campbell <m.campbell92@gmail.com>
2024-11-14 12:27:35 +0100

    add card component, configure props

---
+++ client/src/components/ui/Cards.jsx
@@ -0,0 +1,16 @@
+export default function Card({ imgSrc, imgAlt, cardTitle, cardContent }) {
+  return (
+    <div className="card card-compact bg-base-300 w-full shadow-xl">
+      <figure>
+        <img src={imgSrc} alt={imgAlt} className="w-full h-60 object-cover" />
+      </figure>
+      <div className="card-body text-primary-content">
+        <h2 className="card-title">{cardTitle}</h2>
+        <p>{cardContent}</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-secondary mt-4">Find out more</button>
+        </div>
+      </div>
+    </div>
+  );
+}
