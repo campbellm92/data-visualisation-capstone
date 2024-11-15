b219151409c778024e3497188b662d6d2958c5ef
Matthew Campbell <m.campbell92@gmail.com>
2024-11-15 18:08:10 +0100

    import and style tooltip

--- client/src/sections/data-vis-sections/dashboard/DashboardBarChartsSection.jsx
+++ client/src/sections/data-vis-sections/dashboard/DashboardBarChartsSection.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import BarChartHome from "../../../components/charts/barcharts/BarChartHome";
 import { SelectWithBorderSmall } from "../../../components/ui/Selects";
 import { fields } from "../../../utils/constants";
+import GraphInfoIcon from "../../../components/ui/GraphInfoIcon";
 
 export default function DashboardBarChartsSection() {
   const [year, setYear] = useState(2023);
@@ -21,8 +22,20 @@ export default function DashboardBarChartsSection() {
       {/* Bar Charts Grid */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         {fields.map((field) => (
-          <div key={field}>
+          <div
+            key={field}
+            className="relative p-4 bg-base-300 rounded-md shadow-md"
+          >
             <BarChartHome year={year} dataField={field} />
+            {field && (
+              <div className="absolute top-2 right-2 pointer-events-auto">
+                <GraphInfoIcon
+                  info={
+                    "Average daily rate refers to how much money a room makes on average per day."
+                  }
+                />
+              </div>
+            )}
           </div>
         ))}
       </div>
