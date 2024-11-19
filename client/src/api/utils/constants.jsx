2dac7ad313a8253da1b5ec59a1d13c9a5bde73c7
Gary Cazzulino <gary@caz.email>
2024-11-19 21:03:07 +1000

    First commit of integrated AI Analysis

--- client/src/api/utils/constants.jsx
+++ client/src/api/utils/constants.jsx
@@ -9,4 +9,5 @@ export const kAPI_URL = "http://localhost:3000/api";
 export const kDEFAULT_ERROR_MSG = "API Error - an unspecified error occurred";
 export const kGraphLineStyle = 'smooth';
 export const kOriginDate = '2023-01-01';
-export const kGraphColours = {"Gold Coast": "#ff0000", "Noosa": "#00ff00", "Whitsunday" : "#0000ff", "Cairns" : "#ff00ff"};
+export const kGraphColours = {"Gold Coast": "#ff0000", /*`rgb(var(--tw-primary))`,*/ "Noosa": "#00ff00", "Whitsunday" : "#0000ff", "Cairns" : "#ff00ff"};
+export const kDefaultResponse = "Hi there, please select a default prompt or enter a custom prompt above and then click Analyse to pass your prompt and the selected data to an LLM for analysis.";
