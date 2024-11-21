e4c36d5663abce327aaf0c2e92b118fde28dd41c
Matthew Campbell <m.campbell92@gmail.com>
2024-11-21 16:47:04 +0100

    create colour config file for graphs

---
+++ client/src/api/utils/graphColourConfig.js
@@ -0,0 +1,83 @@
+// colour scheme for the graphs for different LGAs
+// note: because of Recharts restrictions, this cannot be handled in tailwind.config.js
+export const lgaColours = {
+  Cairns: {
+    light: {
+      primary: "#15803d",
+      "primary-content": "#0f172a",
+      secondary: "#0ea5e9",
+      "secondary-content": "#f1f5f9",
+      "base-300": "#f9fafb",
+    },
+    dark: {
+      primary: "#16a34a",
+      "primary-content": "#edebe7",
+      secondary: "#0ea5e9",
+      "secondary-content": "#f1f5f9",
+      "base-300": "#111827",
+    },
+  },
+  Noosa: {
+    light: {
+      primary: "#ab274f",
+      "primary-content": "#0f172a",
+      secondary: "#0ea5e9",
+      "secondary-content": "#f1f5f9",
+      "base-300": "#f9fafb",
+    },
+    dark: {
+      primary: "#d63c6f",
+      "primary-content": "#edebe7",
+      secondary: "#0ea5e9",
+      "secondary-content": "#f1f5f9",
+      "base-300": "#111827",
+    },
+  },
+  Whitsunday: {
+    light: {
+      primary: "#0e7490",
+      "primary-content": "#0f172a",
+      secondary: "#0ea5e9",
+      "secondary-content": "#f1f5f9",
+      "base-300": "#f9fafb",
+    },
+    dark: {
+      primary: "#06b6d4",
+      "primary-content": "#edebe7",
+      secondary: "#0ea5e9",
+      "secondary-content": "#f1f5f9",
+      "base-300": "#111827",
+    },
+  },
+  "Gold Coast": {
+    light: {
+      primary: "#d97706",
+      "primary-content": "#0f172a",
+      secondary: "#0ea5e9",
+      "secondary-content": "#f1f5f9",
+      "base-300": "#f9fafb",
+    },
+    dark: {
+      primary: "#f59e0b",
+      "primary-content": "#edebe7",
+      secondary: "#0ea5e9",
+      "secondary-content": "#f1f5f9",
+      "base-300": "#111827",
+    },
+  },
+};
+
+export const monthColours = {
+  Jan: "#fbbf24",
+  Feb: "#fbbf24",
+  Mar: "#EA580C",
+  Apr: "#EA580C",
+  May: "#EA580C",
+  Jun: "#3B82F6",
+  Jul: "#3B82F6",
+  Aug: "#3B82F6",
+  Sept: "#10B981",
+  Oct: "#10B981",
+  Nov: "#10B981",
+  Dec: "#fbbf24",
+};
