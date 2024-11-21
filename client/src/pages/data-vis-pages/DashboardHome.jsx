ce37f4f8a36c5bfb33f04cdf2c804ab57cc84473
Matthew Campbell <m.campbell92@gmail.com>
2024-11-21 16:49:53 +0100

    change styles of overview text part

--- client/src/pages/data-vis-pages/DashboardHome.jsx
+++ client/src/pages/data-vis-pages/DashboardHome.jsx
@@ -10,8 +10,11 @@ export default function DashboardHome() {
   return (
     <div>
       <div className="pb-5">
-        <h1 className="font-light text-xl">
-          Overview for <span className="text-primary">{userLGA}</span>
+        <h1 className="font-light text-xl text-primary-content">
+          Overview for{" "}
+          <span className="font-bold text-primary-content underline underline-offset-2 decoration-primary decoration-4">
+            {userLGA}
+          </span>
         </h1>
       </div>
       <DashboardBarChartsSection />
