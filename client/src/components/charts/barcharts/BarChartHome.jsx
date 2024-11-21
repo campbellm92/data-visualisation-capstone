ba732bd7773a481f27e4bfe5c695816f1f3e58f0
Matthew Campbell <m.campbell92@gmail.com>
2024-11-21 16:47:29 +0100

    adjust logic for dynamic colours

--- client/src/components/charts/barcharts/BarChartHome.jsx
+++ client/src/components/charts/barcharts/BarChartHome.jsx
@@ -11,8 +11,10 @@ import {
   YAxis,
 } from "recharts";
 import { ThemeContext } from "../../../context/ThemeProvider";
-import { dataFieldDisplayNames, lgaColours } from "../../../utils/constants";
+import { dataFieldDisplayNames } from "../../../api/utils/constants";
+import { lgaColours } from "../../../api/utils/graphColourConfig";
 import { useChartDataConfig } from "../../../api/hooks/useChartDataConfig";
+import CustomTooltip from "../CustomTooltips/CustomTooltip";
 
 export default function BarChartHome({ year, dataField }) {
   const { loading, data, error, user } = useChartDataConfig({
@@ -28,18 +30,27 @@ export default function BarChartHome({ year, dataField }) {
 
   const displayName = dataFieldDisplayNames[dataField] || dataField;
 
-  const fillColour = darkMode
-    ? lgaColours[userLGA].dark
-    : lgaColours[userLGA].light;
+  const colours = lgaColours[userLGA]?.[darkMode ? "dark" : "light"];
+
+  const fillColour = colours.primary;
 
   return (
     <div className="flex flex-wrap justify-center gap-4">
       <div className="p-4 flex justify-center items-center flex-1 min-w-[300px] max-w-[400px]">
         <ResponsiveContainer width="100%" height={300}>
           <BarChart data={data}>
-            <XAxis dataKey="month" tickLine={false} axisLine={false} />
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              axisLine={false}
+              style={{
+                fill: colours["primary-content"],
+              }}
+            />
             <YAxis hide={true} />
-            <Tooltip />
+            <Tooltip
+              content={<CustomTooltip userLGA={userLGA} chartType="bar" />}
+            />
             <Legend iconType="circle" />
             <Bar dataKey={dataField} fill={fillColour} name={displayName} />
           </BarChart>
