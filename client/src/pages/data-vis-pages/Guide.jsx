077b17cfb1772e9b22df7ff375665033f033b106
Amrit Maharjan <amrit.mhrj@gmail.com>
2024-11-23 18:51:42 +0545

    Fixed dark theme accessibility issues for Guide page

--- client/src/pages/data-vis-pages/Guide.jsx
+++ client/src/pages/data-vis-pages/Guide.jsx
@@ -2,24 +2,24 @@ export default function Guide() {
   return (
     <div className="relative" tabIndex={0}>
       <div className="ml-12 mt-6">
-        <h1 className="text-primary font-bold text-4xl">
+        <h1 className="text-primary text-primary-content font-bold text-4xl">
           Localis Data Dashboard: A Quick Guide 🤓
         </h1>
       </div>
       <div className="flex">
         <div className="m-12 flex-1 pr-10">
-          <p className="text-xl">
+          <p className="text-xl text-primary-content">
             Hello, and thanks for trusting us to find the best data insights for
             your local area. The following guide will run you through how to
             understand our data and how you can get the most out of this app.
           </p>
-          <h1 className="text-3xl font-semibold text-primary mt-10" id="adr">
+          <h1 className="text-3xl font-semibold text-primary-content mt-10" id="adr">
             The Data
           </h1>
-          <h2 className="text-xl mt-6 underline decoration-primary">
+          <h2 className="text-xl mt-6 underline decoration-primary text-primary-content">
             Average Daily Rate
           </h2>
-          <p className="mt-3">
+          <p className="mt-3 text-primary-content">
             The Average Daily Rate (ADR) is a key performance metric in the
             accommodation industry that represents the average income earned per
             occupied room in a specific period. It is calculated by dividing the
@@ -27,12 +27,12 @@ export default function Guide() {
             understand pricing trends and revenue performance.
           </p>
           <h2
-            className="text-xl mt-6 underline decoration-primary"
+            className="text-xl mt-6 underline decoration-primary text-primary-content"
             id="occupancy"
           >
             Average Historical Occupancy
           </h2>
-          <p className="mt-3">
+          <p className="mt-3 text-primary-content">
             Historical occupancy refers to the percentage of available rooms or
             properties that were occupied over a given period in the past. It is
             calculated by dividing the total number of rooms or properties
@@ -41,12 +41,12 @@ export default function Guide() {
             businesses track trends in room usage and demand over time.
           </p>
           <h2
-            className="text-xl mt-6 underline decoration-primary"
+            className="text-xl mt-6 underline decoration-primary text-primary-content"
             id="booking-window"
           >
             Average Booking Window
           </h2>
-          <p className="mt-3">
+          <p className="mt-3 text-primary-content">
             The average booking window measures the average number of days
             between when a guest makes a reservation and their check-in date. It
             helps hotels and accommodations understand how far in advance their
@@ -54,12 +54,12 @@ export default function Guide() {
             strategies.
           </p>
           <h2
-            className="text-xl mt-6 underline decoration-primary"
+            className="text-xl mt-6 underline decoration-primary text-primary-content"
             id="length-of-stay"
           >
             Average Length of Stay
           </h2>
-          <p className="mt-3">
+          <p className="mt-3 text-primary-content">
             Average Length of Stay (ALOS) refers to the average number of days
             that guests stay at a property. It is calculated by dividing the
             total number of room nights occupied by the number of bookings or
@@ -68,10 +68,10 @@ export default function Guide() {
           <div className="flex w-full flex-col">
             <div className="divider divider-secondary"></div>
           </div>
-          <h1 className="text-3xl font-semibold text-primary mt-4" id="adr">
+          <h1 className="text-3xl font-semibold text-primary-content mt-4" id="adr">
             How to use this app
           </h1>
-          <p className="mt-3">
+          <p className="mt-3 text-primary-content">
             {" "}
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
             reiciendis qui sed expedita praesentium, quaerat placeat sint
