ab7c16fcd62ff50fd3bc4215ef2116602397c438
Matthew Campbell <m.campbell92@gmail.com>
2024-11-26 19:21:05 +0100

    move state management to context provider

--- client/src/components/layout/AIAnalysisModal.jsx
+++ client/src/components/layout/AIAnalysisModal.jsx
@@ -1,131 +1,110 @@
-import { useState } from "react";
+import { useContext } from "react";
+import { AiAnalysisContext } from "../../context/AiAnalysisProvider";
 import { ButtonMediumFullWide } from "../ui/Buttons";
+import { CloseButton } from "../ui/Buttons";
 import LoadingSpinner from "../ui/LoadingSpinner";
-import LLMResponse from "./LLMResponse";
 import { SelectLLMPrompt } from "../ui/Select";
-import { getLLMResponseFromServer } from "../../api/utils/getUtils";
+import Draggable from "react-draggable";
+// import { getLLMResponseFromServer } from "../../api/utils/getUtils";
 
-function AIAnalysisModal({
-  dataSet,
-  llmResponse,
-  setllmResponse,
-  setAiAnalysisSelected,
-}) {
-  const [loading, setIsLoading] = useState(false);
-  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
-  const [customPrompt, setCustomPrompt] = useState("");
-  const [defaultPrompt, setDefaultPrompt] = useState("");
+export function AIAnalysisModal({ closeModal }) {
+  const {
+    year,
+    loading,
+    isButtonDisabled,
+    customPrompt,
+    setCustomPrompt,
+    defaultPrompt,
+    setDefaultPrompt,
+    LLMResponse: responseContent,
+    doAnalysis,
+    handleSelectChange,
+  } = useContext(AiAnalysisContext);
 
-  const handleSelectChange = () => {
-    setCustomPrompt("");
+  const handleDoAnalysis = (e) => {
+    doAnalysis(e, dataSet);
   };
-
-  const closeForm = () => {
-    setllmResponse("");
-    setAiAnalysisSelected(false);
-  };
-
-  const doAnalysis = async (e) {
-    e.preventDefault();
-    e.stopPropagation();
-
-    setllmResponse("");
-    setIsLoading(true);
-    setIsButtonDisabled(true);
-
-    const promptField = customPrompt || defaultPrompt;
-
-    try {
-      const response = await getLLMResponseFromServer(promptField, dataSet);
-      setllmResponse(response);
-    } catch (error) {
-      setllmResponse(
-        "Too much data selected. Please select less data and try again. Version 2 will fix this issue.",
-        error
-      );
-    } finally {
-      setIsLoading(false);
-      setIsButtonDisabled(false);
-    }
-    setAiAnalysisSelected(true);
-  }
 
   return (
     <div>
-      <dialog id="analyse_modal" className="modal w-full">
-        <div className="shadow-md border-1 rounded p-3 relative shadow-lg bg-base-300">
-          <form id="prompt-form" method="dialog" onSubmit={doAnalysis}>
-            <label
-              id="prompt-label"
-              className="form-control text-primary-content"
-            >
-              <div className="label">
-                <span className="font-bold text-primary-content">
-                  Generate an AI Analysis
-                </span>
-              </div>
-              <div className="label">
-                <span className="text-primary-content">
-                  By using a Default Prompt
-                </span>
-              </div>
-              <SelectLLMPrompt
-                handleSelectChange={handleSelectChange}
-                value={defaultPrompt}
-                onChange={(e) => setDefaultPrompt(e.target.value)}
-              />
-            </label>
-            <label
-              id="prompt-label"
-              className="form-control text-primary-content"
-            >
-              <div className="label">
-                <span className="text-primary-content">
-                  Or Write a Custom Prompt
-                </span>
-              </div>
-              <textarea
-                value={customPrompt}
-                onChange={(e) => setCustomPrompt(e.target.value)}
-                className="textarea textarea-bordered h-24 text-primary-content shadow-lg bg-base-300 max-h-40"
-                placeholder="Write your custom prompt here."
-              ></textarea>
-            </label>
-          </form>
+      <Draggable>
+        <dialog id="analyse_modal" className="modal w-full" open>
+          <div className="shadow-md border-1 rounded p-6 relative shadow-lg bg-base-300 bg-opacity-95">
+            <div className="flex justify-end">
+              <CloseButton onClick={closeModal} />
+            </div>
 
-          {llmResponse && (
-            <label
-              id="response-label"
-              className="form-control mb-5"
-              style={{ display: "none" }}
+            <form id="prompt-form" method="dialog" onSubmit={handleDoAnalysis}>
+              <label
+                id="prompt-label"
+                className="form-control text-primary-content"
+              >
+                <div className="label">
+                  <span className="font-bold text-primary-content pb-4">
+                    Generate an AI analysis of this data...
+                  </span>
+                </div>
+                <div className="label">
+                  <span className="text-primary-content pb-2">
+                    by using a default prompt:
+                  </span>
+                </div>
+                <div className="pb-4">
+                  <SelectLLMPrompt
+                    handleSelectChange={handleSelectChange}
+                    value={defaultPrompt}
+                    onChange={(e) => setDefaultPrompt(e.target.value)}
+                  />
+                </div>
+              </label>
+              <label
+                id="prompt-label"
+                className="form-control text-primary-content"
+              >
+                <div className="label">
+                  <span className="text-primary-content pb-2">
+                    or writing a custom prompt:
+                  </span>
+                </div>
+                <div className="pb-4">
+                  <textarea
+                    value={customPrompt}
+                    onChange={(e) => setCustomPrompt(e.target.value)}
+                    className="textarea textarea-bordered h-24 w-full text-primary-content shadow-lg bg-base-300 max-h-40"
+                    placeholder="Write your custom prompt here."
+                  ></textarea>
+                </div>
+              </label>
+            </form>
+
+            {loading && (
+              <div className="text-center m-2">
+                <LoadingSpinner />
+              </div>
+            )}
+
+            <ButtonMediumFullWide
+              onClick={handleDoAnalysis}
+              disabled={isButtonDisabled}
+              className={
+                isButtonDisabled ? "opacity-50 cursor-not-allowed" : ""
+              }
+              textColor="text-secondary-content"
             >
+              Analyse
+            </ButtonMediumFullWide>
+          </div>
+
+          {responseContent && (
+            <label id="response-label" className="form-control mb-5">
               <div className="label">
                 <span className="text-primary-content">LLM Response</span>
               </div>
-              <div className="shadow-lg overflow-scroll bg-base-300 min-h-30 max-h-40">
-                <LLMResponse content={llmResponse} />
-              </div>
+              <div className="shadow-lg overflow-scroll bg-base-300 min-h-30 max-h-40"></div>
             </label>
           )}
-
-          {loading && (
-            <div className="text-center m-2">
-              <LoadingSpinner />
-            </div>
-          )}
-
-          <ButtonMediumFullWide
-            onClick={doAnalysis}
-            disabled={isButtonDisabled}
-            className={isButtonDisabled ? "opacity-50 cursor-not-allowed" : ""}
-            textColor="text-secondary-content"
-          >
-            Analyse
-          </ButtonMediumFullWide>
-        </div>
-      </dialog>
+        </dialog>
+      </Draggable>
     </div>
   );
 }
-
-export default AIAnalysis;
