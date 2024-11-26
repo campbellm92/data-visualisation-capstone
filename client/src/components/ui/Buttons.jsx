69ee7d6587a9d19153d2716198ffe930d4101b25
Matthew Campbell <m.campbell92@gmail.com>
2024-11-26 16:07:24 +0100

    begin work on ai analysis button

--- client/src/components/ui/Buttons.jsx
+++ client/src/components/ui/Buttons.jsx
@@ -47,7 +47,7 @@ export function ButtonMediumFullWide({
   onClick,
   id,
   textColor = "text-primary-content",
-  disabled
+  disabled,
 }) {
   return (
     <button
@@ -70,5 +70,24 @@ export function ButtonOutlineFullWide({ children, onClick, id }) {
     >
       {children}
     </button>
+  );
+}
+
+export function ButtonOpenAIAnalysisModal({ onClick }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="size-8 text-primary-content hover:text-primary cursor-pointer"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M8.25 3v1.5M4.5 8.25H3m18 0h-1.5M4.5 12H3m18 0h-1.5m-15 3.75H3m18 0h-1.5M8.25 19.5V21M12 3v1.5m0 15V21m3.75-18v1.5m0 15V21m-9-1.5h10.5a2.25 2.25 0 0 0 2.25-2.25V6.75a2.25 2.25 0 0 0-2.25-2.25H6.75A2.25 2.25 0 0 0 4.5 6.75v10.5a2.25 2.25 0 0 0 2.25 2.25Zm.75-12h9v9h-9v-9Z"
+      />
+    </svg>
   );
 }
