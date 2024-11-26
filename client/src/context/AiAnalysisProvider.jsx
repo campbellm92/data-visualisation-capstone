245495a923b16e534a833db7c574dcd8c2a7d201
Matthew Campbell <m.campbell92@gmail.com>
2024-11-26 16:07:35 +0100

    begin work on ai analysis provider

---
+++ client/src/context/AiAnalysisProvider.jsx
@@ -0,0 +1,60 @@
+import { createContext, useState } from "react";
+import { getLLMResponseFromServer } from "../api/utils/getUtils";
+
+const AiAnalysisContext = createContext();
+
+export function AiAnalysisProvider({ children }) {
+  const [year, setYear] = useState(2023);
+  const [loading, setIsLoading] = useState(false);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+  const [customPrompt, setCustomPrompt] = useState("");
+  const [defaultPrompt, setDefaultPrompt] = useState("");
+  const [LLMResponse, setLLMResponse] = useState("");
+
+  async function doAnalysis(dataSet) {
+    dataSet.preventDefault();
+    dataSet.stopPropagation();
+    setLLMResponse("");
+    setIsLoading(true);
+    setIsButtonDisabled(true);
+
+    const promptField = customPrompt || defaultPrompt;
+
+    try {
+      const response = await getLLMResponseFromServer(promptField, dataSet);
+      setLLMResponse(response);
+    } catch (error) {
+      setLLMResponse(
+        "Too much data selected. Please select less data and try again. Version 2 will fix this issue.",
+        error
+      );
+    } finally {
+      setIsLoading(false);
+      setIsButtonDisabled(false);
+    }
+  }
+  const closeForm = () => {
+    setLLMResponse("");
+    setCustomPrompt("");
+    setDefaultPrompt("");
+  };
+  return (
+    <AiAnalysisContext.Provider
+      value={{
+        year,
+        setYear,
+        loading,
+        isButtonDisabled,
+        customPrompt,
+        setCustomPrompt,
+        defaultPrompt,
+        setDefaultPrompt,
+        LLMResponse,
+        doAnalysis,
+        closeForm,
+      }}
+    >
+      {children}
+    </AiAnalysisContext.Provider>
+  );
+}
