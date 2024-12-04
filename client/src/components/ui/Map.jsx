4fe1d6bb9af38eccdc38348a6ce7b74c0a7f3119
Matthew Campbell <m.campbell92@gmail.com>
2024-12-04 14:50:23 +0100

    adjust styling

--- client/src/components/ui/Map.jsx
+++ client/src/components/ui/Map.jsx
@@ -1,15 +1,20 @@
-
-export default function Map({location}) {
-    return (
-    <iframe
-        width={400}
+export default function Map({ location }) {
+  return (
+    <div className="w-full max-w-md lg:max-w-lg">
+      <iframe
+        width="100%"
         height={300}
-        className="chart"
-        style={{border:0}}
+        className="rounded-md shadow-md"
+        style={{ border: 0, maxWidth: "600px" }}
         loading="lazy"
         allowfullscreen
-        src={`https://www.google.com/maps?q=${location === 'Whitsunday' ? 'Whitsundays' : location}&output=embed`}
-        aria-label={`Map showing the location of ${location === 'Whitsunday' ? 'Whitsundays' : location}`}
-        >
-    </iframe>);
+        src={`https://www.google.com/maps?q=${
+          location === "Whitsunday" ? "Whitsundays" : location
+        }&output=embed`}
+        aria-label={`Map showing the location of ${
+          location === "Whitsunday" ? "Whitsundays" : location
+        }`}
+      ></iframe>
+    </div>
+  );
 }
