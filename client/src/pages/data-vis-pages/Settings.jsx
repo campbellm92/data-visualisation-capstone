9c71d2ee709890d58d4f10704dc62c02f9f11f12
Amrit Maharjan <amrit.mhrj@gmail.com>
2024-12-08 16:53:13 +1000

    Added Delete Account section

--- client/src/pages/data-vis-pages/Settings.jsx
+++ client/src/pages/data-vis-pages/Settings.jsx
@@ -3,12 +3,14 @@ import { useState, useEffect } from "react";
 import AccountDetails from "../../sections/data-vis-sections/settings/AccountDetails";
 import BillingInformation from "../../sections/data-vis-sections/settings/BillingInformation";
 import { useFetchUserData } from "../../../src/api/hooks/useFetchUserData";
+import DeleteAccount from "../../sections/data-vis-sections/DeleteAccount/DeleteAccount";
 
 const Settings = () => {
   // Object to keep track of open sections
   const [openSections, setOpenSections] = useState({
     accountDetails: true,
     billingInformation: true,
+    deleteAccount: true,
   });
 
   // Function to toggle the section
@@ -70,6 +72,22 @@ const Settings = () => {
           </div>
 
           <BillingInformation data={data} />
+        </div>
+      </div>
+      <div className="ml-10">
+        <div
+          className={`collapse ${
+            openSections.deleteAccount ? "collapse-open" : "collapse-close"
+          } bg-base-200 collapse-arrow lg:w-[900px] sm:w-full`}
+        >
+          <div
+            className="collapse-title text-xl font-medium cursor-pointer text-primary-content"
+            onClick={() => toggleSection("deleteAccount")}
+          >
+            Delete Account
+            <div className="bg-black h-[1.5px] mt-2 mb-0 bg-primary-content"></div>
+          </div>
+          <DeleteAccount />
         </div>
       </div>
     </>
