d33d5da802c5eee918f026d1edcb1c983b8c52f3
Matthew Campbell <m.campbell92@gmail.com>
2024-12-12 09:55:10 +0100

    fix navigate to landing page after logout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -83,7 +83,7 @@ function App() {
 
 function ProtectedRoute({ children }) {
   const { isLoggedIn } = useContext(AuthContext);
-  return isLoggedIn ? children : <Navigate to="/dashboard" />;
+  return isLoggedIn ? children : <Navigate to="/" />;
 }
 
 export default App;
