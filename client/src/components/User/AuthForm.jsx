ee8253cdd5b479a040e86ecbdecdabeff5255200
Matthew Campbell <m.campbell92@gmail.com>
2024-12-15 23:26:47 +0100

    update imports

--- client/src/components/User/AuthForm.jsx
+++ client/src/components/User/AuthForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import Login from "./Login/Login";
-import Register from "./Register/Register";
+import Login from "./Login/login";
+import Register from "./Register/register";
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true); // State to toggle between login and register
